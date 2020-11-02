d36b6061f5c0fa79f65552e3681e09733917883c
imperative-scientist-4600 <nelom3@gmail.com>
2020-11-02 16:09:28 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,58 +4,44 @@ require 'pry'
 
 class Scraper
   
-  #student: doc.css("div.student-card").last
+  #student: doc.css("div.student-card")
   #name: student.css("h4.student-name").text
   #location: student.css("p.student-location").text
   #profile_url: student.css("a").attribute("href").value
 
   def self.scrape_index_page(index_url)
     students = []
-    #student = {}
-    #html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/")
-    #doc = Nokogiri::HTML(html)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-
     doc.css("div.student-card").each do |s|
       student = {}
       student[:name] = s.css("h4.student-name").text
       student[:location] = s.css("p.student-location").text
       student[:profile_url] = s.css("a").attribute("href").value
-    
       students << student
     end
-      
     students
   end
-  
-  
-  #social: doc.css("div.social-icon-container a").attribute("href").value
-  # .split(/\/\/|\./)[1].to_sym
+ 
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
     student = {}
-    doc.css("div.social-icon-container a").each do |media|
-      y = media.attribute("href").value
-      if y.include?("twitter")
-        student[:twitter] = y
-      elsif y.include?("linkedin")
-        student[:linkedin] = y
-      elsif y.include?("git")
-        student[:github] = y
+    doc.css("div.social-icon-container a").each do |s|
+      site = s.attribute("href").value
+      if site.include?("twitter")
+        student[:twitter] = site
+      elsif site.include?("linkedin")
+        student[:linkedin] = site
+      elsif site.include?("git")
+        student[:github] = site
       else
-        student[:blog] = y
+        student[:blog] = site
       end
-      #z = y.split(/\/\/|\./)[1].to_sym
-      #student[y.to_sym] = y
     end
     student[:profile_quote] = doc.css("div.profile-quote").text
     student[:bio] = doc.css("div.description-holder p").text
-    #binding.pry
     student
-    #binding.pry
   end
-
 end
 

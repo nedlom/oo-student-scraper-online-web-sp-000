68b77deeebdb68ed75a6e38df5a1aaa48106120d
imperative-scientist-4600 <nelom3@gmail.com>
2021-04-05 17:16:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,11 +7,11 @@ class Student
   def initialize(student_hash)
     binding.pry
     student_hash.each{|key, value| self.send(("#{key}="), value)}
-    binding.pr
     self.class.all << self
   end
 
   def self.create_from_collection(students_array)
+    binding.pry
     students_array.each do |s|
       self.new(s)
     end
@@ -19,6 +19,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
+    binding.pry
     attributes_hash.each{|key, value| self.send(("#{key}="), value)}
   end
 
